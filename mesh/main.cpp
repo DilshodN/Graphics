ea0073b31f862fe427cb130acdc3eb1e9954ad11
DilshodN <dilshod8471@gmail.com>
2022-05-04 13:50:38 +0700

    7th task

--- mesh/main.cpp
+++ mesh/main.cpp
@@ -1,63 +1,18 @@
-#include <GL/>
-#include <GL/gl.h>
-#include <cstdio.h>
+#include "widget.h"
 
-#include "reader.h"
+#include <QApplication>
 
-Reader obj;
-
-float angle;
-
-void init(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-    glutInitWindowSize(800, 600);
-    glutInitWindowPosition(150, 150);
-    glutCreateWindow("Generic RPG 3D");
+    QApplication a(argc, argv);
 
-    AllocConsole();
-    freopen( "CON", "wt", stdout );
-    freopen( "CON", "wt", stderr );
+    //Для сглаживания//
+    QSurfaceFormat fmt;
+    fmt.setSamples(16);
+    QSurfaceFormat::setDefaultFormat(fmt);
+    /**/
 
-    obj.load("cube.obj");
-
-    glEnable (GL_DEPTH_TEST);
-    glEnable (GL_LIGHTING);
-    glEnable (GL_LIGHT0);
-}
-
-void display(void)
-{
-    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
-    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glLoadIdentity();
-
-    glTranslatef(0.0f, -2.75f, -5.0f);
-    obj.draw("cube.obj");
-
-    glutSwapBuffers();
-    angle += .1f;
-}
-
-void reshape( int width, int height )
-{
-    glViewport(0, 0, (GLsizei)width, (GLsizei)height );
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(60, (GLfloat)width / (GLfloat)height, 1.0, 100.0);
-    glMatrixMode(GL_MODELVIEW);
-}
-
-int main(int argc, char **argv)
-{
-
-    init(argc, argv);
-
-    glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glutIdleFunc(display);
-    glutMainLoop();
-
-    return 0;
+    MyWidget w;
+    w.show();
+    return a.exec();
 }
